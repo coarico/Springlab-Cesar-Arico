08d7516d15ec9e0a73480eec394d4585cc806d39
Cesar Arico <cesar.arico@espe.edu.ec>
2025-11-16 18:40:04 -0500

    Provocar error en CI para prueba - sintaxis faltante

--- src/main/java/edu/espe/springlab/SpringLabApplication.java
+++ src/main/java/edu/espe/springlab/SpringLabApplication.java
@@ -8,6 +8,8 @@ public class SpringLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLabApplication.class, args);
+		// ERROR INTENCIONAL PARA PROBAR CI
+		String error = "esto causará compilación fallida"
 	}
 
 }
